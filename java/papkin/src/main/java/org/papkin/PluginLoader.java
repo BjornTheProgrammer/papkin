1bfe11c63cde336baed9f703029a980a66450ec3
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2026-01-20 10:05:03 -0700

    Add error printing for plugin loader

--- java/papkin/src/main/java/org/papkin/PluginLoader.java
+++ java/papkin/src/main/java/org/papkin/PluginLoader.java
@@ -1,6 +1,5 @@
-package org.papkin;
+package org.papkin.loader;
 
-import io.papermc.paper.plugin.provider.util.ProviderUtil;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -8,15 +7,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class PluginLoader {
 
-    public static JavaPlugin load(String jarPath, String mainClass)
-        throws Exception {
-        File jarFile = new File(jarPath);
-        URL[] urls = new URL[] { jarFile.toURI().toURL() };
-        URLClassLoader classLoader = new URLClassLoader(
-            urls,
-            PluginLoader.class.getClassLoader()
-        );
+    public static Object load(String jarPath, String mainClass) {
+        try {
+            File jarFile = new File(jarPath);
+            URL[] urls = new URL[] { jarFile.toURI().toURL() };
+            URLClassLoader classLoader = new URLClassLoader(
+                urls,
+                PluginLoader.class.getClassLoader()
+            );
 
-        return ProviderUtil.loadClass(mainClass, JavaPlugin.class, classLoader);
+            Class<?> pluginClass = classLoader.loadClass(mainClass);
+            Object instance = pluginClass
+                .getDeclaredConstructor()
+                .newInstance();
+
+            // Note: This won't work properly because JavaPlugin
+            // requires init() to be called first with proper parameters
+            return instance;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
